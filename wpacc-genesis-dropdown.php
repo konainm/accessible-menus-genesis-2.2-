5cdf544bc34c26a5d6238195c663a5ca0acb1e62
Konain Mukadam <kmm976@nyu.edu>
2018-08-09 15:45:06 -0400

    Pushed Mark's suggestion
    Changed from if else logic to select theme to genesis_setup hook

--- wpacc-genesis-dropdown.php
+++ wpacc-genesis-dropdown.php
@@ -3,23 +3,18 @@
  * Plugin Name: Accessible Menus for Genesis Framework (Genesis 2.2+)
  * Plugin URI: https://github.com/KonainM/accessible-menus-genesis-2.2-.git
  * Description: Adds keyboard accessibility to Genesis framework's menus (Supports Genesis 2.2+)
- * Version: 1.1
+ * Version: 1.15
  * Author: Konain Mukadam
  * Author URI: https://github.com/KonainM/
 */
 
 
-add_action( 'wp_enqueue_scripts', 'wpacc_genesis_dropdown_scripts' );
+add_action( 'genesis_setup', 'wpacc_genesis_dropdown_scripts', 15 );
 function wpacc_genesis_dropdown_scripts()
 {
-	$my_theme = wp_get_theme();
-	if ($my_theme->get( 'Template' ) === "genesis")
-	{
 		wp_enqueue_script( 'wpacc-genesis-dropdown',   plugins_url( '/wpacc-genesis-dropdown.js' , __FILE__ ), array( 'jquery' ), false, true );
-
 		wp_register_style( 'wpacc-genesis-dropdown-css', plugins_url( '/wpacc-genesis-dropdown.css' , __FILE__ ) );
 		wp_enqueue_style('wpacc-genesis-dropdown-css');
-	}
 }
 
 
